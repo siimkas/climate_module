fae2239ca2426079fe07fec87a262a95b80e5e02
siimkas <kasela.siim@gmail.com>
2021-12-16 19:14:33 +0200

    Update v3.ino

--- v3.ino
+++ v3.ino
@@ -1,7 +1,7 @@
 /*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
+    
+    Based on : https://www.instructables.com/ESP32-BLE-Android-App-Arduino-IDE-AWESOME/
+    
 
    Create a BLE server that, once we receive a connection, will send periodic notifications.
    The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
